50c1cc79c11891a9d42cfc763f77b5ba626dc8fd
lfschultz <lfschultz@ymail.com>
2017-11-07 19:03:49 -0600

    Added in some WriteLines explaining some game commands/inputs.

--- Program.cs
+++ Program.cs
@@ -12,11 +12,20 @@ namespace beg_aunt_bertha
         {
             string answer;
 
+            Console.WriteLine("Welcome to Beg Aunt Bertha.");
+            Console.WriteLine("Type P to start the game.");
+            Console.WriteLine("The following values can be keyed in at any time to do the following:");
+            Console.WriteLine("O - Read the objective for the game.");
+            Console.WriteLine("S - Check the player's status.");
+            Console.WriteLine("Q - Quit the game.");
+
+            Console.Write("Enter in a value to proceed: ");
             answer = Convert.ToString(Console.Read()).ToUpper();
 
             while (answer != "Q")
             {
-                answer = Convert.ToString(Console.Read());
+                Console.Write("What will you do next? Enter in a command: ");
+                answer = Convert.ToString(Console.Read()).ToUpper();
             }
 
             Console.WriteLine("Thanks for playing!");

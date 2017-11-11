ae4a563d9d4f15ace17493aa95888b6ad2eb0e11
lfschultz <lfschultz@ymail.com>
2017-11-11 17:12:14 -0600

    Added in variables and comments on what they will do.

--- GameData.cs
+++ GameData.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace beg_aunt_bertha
 {
+    // This class will be subject to a lot of changes as the game evolves. 
+    // It is possible that it will be split into more than one class.
     class GameData
     {
+        // Variables
+        TimeSpan currentTime = new TimeSpan(); // Tracks current time in game.
+        int badSeed; // Controls Aunt Bertha's difficulty.
+        int boredom; // Tracks the player's boredom level. 
+        int anger; // Aunt Bertha's tolerance of the player's antics.
+        string berthaStatus; // Aunt Bertha's current status. 
+        string weather; // Set at the beginning and (for now) changes halfway through the game.
     }
 }

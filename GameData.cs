2a84346dd05e24f09fb108185403743f51485def
= <lfschultz@ymail.com>
2017-12-17 18:39:56 -0600

    Further restructuring with class variables vs. getters/setters.

--- GameData.cs
+++ GameData.cs
@@ -11,23 +11,6 @@ namespace beg_aunt_bertha
     class GameData
     {
         // Variables
-        private TimeSpan currentTime; // Tracks current time in game.
-        private int badSeed; // Controls Aunt Bertha's difficulty.
-        private int boredom; // Tracks the player's boredom level. 10 = insanity by boredom/game over.
-        private int anger; // Aunt Bertha's tolerance of the player's antics. 10 = early bedtime/game over.
-        private string berthaStatus; // Aunt Bertha's current status. 
-        private string weather; // Set at the beginning and (for now) changes halfway through the game.
-
-        public GameData() // Sets defaults when game starts.
-        {
-            currentTime = new TimeSpan(13,0,0);
-            badSeed = 1;
-            boredom = 1;
-            anger = 1;
-            berthaStatus = "Watching TV";
-            weather = "Sunny";
-        }
-
         public TimeSpan CurrentTime { get; set; }
         public int BadSeed { get; set; }
         public int Boredom { get; set; }
@@ -35,6 +18,16 @@ namespace beg_aunt_bertha
         public string BerthaStatus { get; set; }
         public string Weather { get; set; }
 
+        public GameData() // Sets defaults when game starts.
+        {
+            CurrentTime = new TimeSpan(13,0,0);
+            BadSeed = 1;
+            Boredom = 1;
+            Anger = 1;
+            BerthaStatus = "Watching TV";
+            Weather = "Sunny";
+        }
+        
         //////// Class methods ////////
 
         /* This method randomly determines the weather in the game. For now, it is only

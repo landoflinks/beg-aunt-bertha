5a3e7f094b98f38b45094be7cfa86d1693ca8f7e
= <lfschultz@ymail.com>
2018-01-06 17:51:50 -0600

    Revised the ordering of a lot of the main() method in order to try and prepare for the game loop to be more functional and user-friendly.

--- Program.cs
+++ Program.cs
@@ -17,7 +17,12 @@ namespace beg_aunt_bertha
             Console.WriteLine("Welcome to Beg Aunt Bertha.");
             HelpText();
 
-            Console.Write("Enter in a value: ");
+            // Setting pre-game stats
+            stats.BadSeed = stats.SetDifficulty();
+            stats.Weather = stats.SetWeather();
+            stats.BerthaStatus = stats.SetBerthaStatus();
+
+                Console.Write("Enter in a value: ");
             answer = Convert.ToString(Console.ReadLine()).ToUpper();
 
             while (answer != "Q")
@@ -25,26 +30,8 @@ namespace beg_aunt_bertha
                 switch (answer)
                 {
                     case "O":
-                        // Game objective
-                        Console.WriteLine("It's the first day of summer vacation. You're looking forward " +
-                            "to an uninterrupted day full of video game fun! But then, disaster strikes. " +
-                            "Your mom gets called into work. Guess who has volunteered to babysit you? " +
-                            "Your worst nightmare, your least favorite relative...your great-aunt Bertha!");
-                        Console.WriteLine("");
-                        Console.WriteLine("Aunt Bertha is one of THOSE old ladies. You know the type...." +
-                            "She thinks that video games rot your brain, contribute to all sorts of " +
-                            "problems in society, and in general shouldn't exist.");
-                        Console.WriteLine("");
-                        Console.WriteLine("She's also quite boring. Aunt Bertha is quite content to sit " +
-                            "in the living room most of the day watching daytime soap operas or poring " +
-                            "through stale old romance novels. She never does anything interesting!");
-                        Console.WriteLine("");
-                        Console.WriteLine("Your mission, should you choose to accept it, is to salvage " +
-                            "what precious game time you can during your afternoon. Through " +
-                            "stealth or rebellion, you will play games! And, if neither of those work, " +
-                            "you will have to beg Aunt Bertha!");
-                        Console.WriteLine("Press any key to continue.");
-                        Console.ReadKey();
+                        // Calls the self-explanatory Objective method.
+                        Objective();
                         break;
                     case "H":
                         // Calls the HelpText method to display helpful commands.
@@ -54,21 +41,8 @@ namespace beg_aunt_bertha
                         // Game stats display here.
                         stats.DisplayStats();
                         break;
-                    case "P":
-                        if (gameSetup == true)
-                        {
-                            // Setting pre-game stats
-                            gameSetup = false;
-                            stats.BadSeed = stats.SetDifficulty();
-                            stats.Weather = stats.SetWeather();
-                            stats.BerthaStatus = stats.SetBerthaStatus();
-                        }
-                        
-                        // Main game loop
-
-                        break;
-
                 }
+
                 Console.Write("Enter in a command: ");
                 answer = Convert.ToString(Console.ReadLine()).ToUpper();
             }
@@ -80,12 +54,35 @@ namespace beg_aunt_bertha
         protected static void HelpText()
         {
             // This method displays helpful commands.
-            Console.WriteLine("Type P to start the game.");
             Console.WriteLine("The following values can be keyed in at any time to do the following:");
             Console.WriteLine("O - Read the objective for the game.");
             Console.WriteLine("S - Check the current stats in the game.");
             Console.WriteLine("H - Display helpful commands.");
             Console.WriteLine("Q - Quit the game.");
+        }
+
+        protected static void Objective()
+        {
+            // This explains the objective of the game.
+            Console.WriteLine("It's the first day of summer vacation. You're looking forward " +
+                            "to an uninterrupted day full of video game fun! But then, disaster strikes. " +
+                            "Your mom gets called into work. Guess who has volunteered to babysit you? " +
+                            "Your worst nightmare, your least favorite relative...your great-aunt Bertha!");
+            Console.WriteLine("");
+            Console.WriteLine("Aunt Bertha is one of THOSE old ladies. You know the type...." +
+                "She thinks that video games rot your brain, contribute to all sorts of " +
+                "problems in society, and in general shouldn't exist.");
+            Console.WriteLine("");
+            Console.WriteLine("She's also quite boring. Aunt Bertha is quite content to sit " +
+                "in the living room most of the day watching daytime soap operas or poring " +
+                "through stale old romance novels. She never does anything interesting!");
+            Console.WriteLine("");
+            Console.WriteLine("Your mission, should you choose to accept it, is to salvage " +
+                "what precious game time you can during your afternoon. Through " +
+                "stealth or rebellion, you will play games! And, if neither of those work, " +
+                "you will have to beg Aunt Bertha!");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
         }
     }
 }

ada0490e4f5bdac292e7b7aef34d65f96c546f86
= <lfschultz@ymail.com>
2018-01-17 17:46:31 -0600

    Added a brief message at startup about pressing a key to continue.

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace beg_aunt_bertha
 
             Console.WriteLine("Welcome to Beg Aunt Bertha.");
             HelpText();
+            Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
 
             // Setting pre-game stats
